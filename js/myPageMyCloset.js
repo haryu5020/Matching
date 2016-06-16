a5e74036b1c2917925f378b9459af77dc4cfbec6
HyunJun Sung <HyunJun Sung>
2016-06-17 08:14:11 +0900

    closet

--- js/myPageMyCloset.js
+++ js/myPageMyCloset.js
@@ -7,3 +7,20 @@ $(".closetList").click(function(){
     $("#bottomView > img").attr("src", under);
 })
 
+$("#cancel").click(function(){
+    var top=$("#manekin > .top").children("img").attr("src");
+    var under=$("#manekin > .under").children("img").attr("src");
+    
+    $("#manekin > .top").children("img").attr("src", " ");
+    $("#manekin > .under").children("img").attr("src", " ");
+    $("#upView > img").attr("src", " ");
+    $("#bottomView > img").attr("src", " ");
+    
+    $(".closetList").each(function(){
+        var chk=$(this).children(".top").children("img").attr("src");
+        if(chk==top){
+            $(this).children(".top").children("img").attr("src", " ");
+            $(this).children(".under").children("img").attr("src", " ");
+        }
+    })
+})
